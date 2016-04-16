0764cf0af788afbf3be28d68a1cc2fdfd3ce9ef2
Matthew J. Dovey <m.dovey@ceridwen.com>
2016-04-16 19:43:51 +0100

    Add swagger-2.1.4 ui

--- lcfserver-frontend-restlet-module-swagger/src/main/java/com/ceridwen/lcf/server/frontend/restlet/modules/SwaggerModule.java
+++ lcfserver-frontend-restlet-module-swagger/src/main/java/com/ceridwen/lcf/server/frontend/restlet/modules/SwaggerModule.java
@@ -22,6 +22,9 @@
 package com.ceridwen.lcf.server.frontend.restlet.modules;
 
 import org.restlet.Application;
+import org.restlet.Client;
+import org.restlet.data.Protocol;
+import org.restlet.resource.Directory;
 import org.restlet.ext.swagger.Swagger2SpecificationRestlet;
 import org.restlet.routing.Router;
 
@@ -30,19 +33,33 @@ import com.ceridwen.lcf.server.frontend.restlet.core.OptionalModuleConfiguration
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 import com.wordnik.swagger.util.Json;
 import com.wordnik.swagger.util.Yaml;
+import javax.servlet.ServletContext;
+
 
 public class SwaggerModule implements OptionalModuleConfiguration {
 
 	@Override
 	public void initialise(Application application, Router router) {
 		Json.mapper().registerModule(new JaxbAnnotationModule());
-        Yaml.mapper().registerModule(new JaxbAnnotationModule());
+    Yaml.mapper().registerModule(new JaxbAnnotationModule());
 
-        // Configuring Swagger 2 support
-        Swagger2SpecificationRestlet swagger2SpecificationRestlet
-                       = new Swagger2SpecificationRestlet(application);
-        swagger2SpecificationRestlet.setBasePath(
-                               "http://lcf.ceridwen.com/restlet");
-        swagger2SpecificationRestlet.attach(router, "/api-docs/swagger.json");
-	}
+    String path = "http://localhost:8000/lcfserver";
+    try {
+      ServletContext sc = (ServletContext) application.getContext().getServerDispatcher().getContext().getAttributes().get( "org.restlet.ext.servlet.ServletContext" );
+      path = sc.getResource("").toString();        
+    } catch (Exception ex) {
+    }
+    
+    // Configuring Swagger 2 support
+    Swagger2SpecificationRestlet swagger2SpecificationRestlet
+                   = new Swagger2SpecificationRestlet(application);
+    swagger2SpecificationRestlet.setBasePath(path);
+    swagger2SpecificationRestlet.attach(router, "/api-docs/swagger.json");
+    
+    Directory swaggerUI = new Directory(application.getContext(), "clap://classloader/swagger-2.1.4/");
+    swaggerUI.setIndexName("index.html");
+    application.getContext().setClientDispatcher(new Client(application.getContext(), Protocol.CLAP));
+    
+    router.attach("/swagger/", swaggerUI);
+  }
 }
